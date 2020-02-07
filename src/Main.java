9e46c05794b171eee6088ed900579d80b19c7bbf
david <david.m.m.antunes@gmail.com>
2020-02-07 15:47:06 +0000

    Added command loop

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.print(">");
+		String option = getCommand(in);
+		while (!option.equals("exit")) {
+			execOption(in, option);
+		}
 	}
 
+	private static String getCommand(Scanner in) {
+		return in.next().trim().toLowerCase();
+	}
+	
+	private static void execOption(Scanner in, String option) {
+		
+		switch(option) {
+		case "register":
+			
+			break;
+		case "login":
+		
+			break;
+		
+		case "help":
+			
+			break;
+			
+		default:
+			
+		}
+	}
 }
